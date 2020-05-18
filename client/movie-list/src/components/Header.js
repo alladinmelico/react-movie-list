9a099ba0f4a208d020cdc7b9347e057c87b5a794
alladinMelico <melico.alladin@gmail.com>
2020-05-18 08:55:27 +0800

    fixed header

--- client/movie-list/src/components/Header.js
+++ client/movie-list/src/components/Header.js
@@ -4,11 +4,11 @@ const Header = () => {
     return (
         <nav>
             <ul id="menu-list">
-                <li><a href="">Home</a></li>
-                <li><a href="">Favorites</a></li>
+                <li><a href="/">Movies</a></li>
+                <li><a href="/">Favorites</a></li>
             </ul>
             <div id="login">
-                <a href="">Login</a>
+                <a href="/">Login</a>
             </div>
         </nav>
     )
